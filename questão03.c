2f063ca64bc98326ac8a883269b2d4d70ba85c09
SandLosT <142359558+SandLosT@users.noreply.github.com>
2023-11-07 18:20:35 -0300

    Update questão03.c

--- questão03.c
+++ questão03.c
@@ -1,52 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef int teirin;
+
+void lerMatriz(teirin matriz[4][4]) {
+    for (int linha = 0; linha < 4; linha++) {
+        for (int coluna = 0; coluna < 4; coluna++) {
+            scanf("%d", &matriz[linha][coluna]);
+        }
+    }
+}
+
+void somarMatrizes(teirin mat01[4][4], teirin mat02[4][4], teirin resultado[4][4]) {
+    for (int linha = 0; linha < 4; linha++) {
+        for (int coluna = 0; coluna < 4; coluna++) {
+            resultado[linha][coluna] = mat01[linha][coluna] + mat02[linha][coluna];
+        }
+    }
+}
+
+void subtrairMatrizes(teirin mat01[4][4], teirin mat02[4][4], teirin resultado[4][4]) {
+    for (int linha = 0; linha < 4; linha++) {
+        for (int coluna = 0; coluna < 4; coluna++) {
+            resultado[linha][coluna] = mat01[linha][coluna] - mat02[linha][coluna];
+        }
+    }
+}
+
+void multiplicarMatrizes(teirin mat01[4][4], teirin mat02[4][4], teirin resultado[4][4]) {
+    for (int linha = 0; linha < 4; linha++) {
+        for (int coluna = 0; coluna < 4; coluna++) {
+            resultado[linha][coluna] = 0;
+            for (int k = 0; k < 4; k++) {
+                resultado[linha][coluna] += mat01[linha][k] * mat02[k][coluna];
+            }
+        }
+    }
+}
+
 int main() {
-    typedef int teirin;
     teirin mat01[4][4];
     teirin mat02[4][4];
     char operacao[5];
     teirin resultado[4][4];
     
-    for (int linha = 0; linha < 4; linha++) {
-        for (int coluna = 0; coluna < 4; coluna++) {
-            scanf("%d", &mat01[linha][coluna]);
-        }
-    }
-    
-    for (int linha = 0; linha < 4; linha++) {
-        for (int coluna = 0; coluna < 4; coluna++) {
-            scanf("%d", &mat02[linha][coluna]);
-        }
-    }scanf("%s", operacao);
-
+    lerMatriz(mat01);
+    lerMatriz(mat02);
+    scanf("%s", operacao);
 
     if (strcmp(operacao, "soma") == 0) {
-        for (int linha = 0; linha < 4; linha++) {
-            for (int coluna = 0; coluna < 4; coluna++) {
-                resultado[linha][coluna] = mat01[linha][coluna] + mat02[linha][coluna];
-            }
-        }
+        somarMatrizes(mat01, mat02, resultado);
     }
     else if (strcmp(operacao, "sub") == 0) {
-        for (int linha = 0; linha < 4; linha++) {
-            for (int coluna = 0; coluna < 4; coluna++) {
-                resultado[linha][coluna] = mat01[linha][coluna] - mat02[linha][coluna];
-            }
-        }
+        subtrairMatrizes(mat01, mat02, resultado);
     }
     else if (strcmp(operacao, "mult") == 0) {
-        for (int linha = 0; linha < 4; linha++) {
-            for (int coluna = 0; coluna < 4; coluna++) {
-                resultado[linha][coluna] = 0;
-                for (int k = 0; k < 4; k++) {
-                    resultado[linha][coluna] += mat01[linha][k] * mat02[k][coluna];
-                }
-            }
-        }
+        multiplicarMatrizes(mat01, mat02, resultado);
     }
 
-    
     for (int linha = 0; linha < 4; linha++) {
         for (int coluna = 0; coluna < 4; coluna++) {
             printf("%*d", +4, resultado[linha][coluna]);
